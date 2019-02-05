2f87fa74488511ce91d76a665d58141e78ceeb95
Kirsten Werner <kirstenmwerner@gmail.com>
2019-02-05 07:18:06 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array)
    i = 0
    new_array = []
   while (i < array.length) do 
-    yield new_array.push(array[i]) 
+    new array << yield array[i] 
     i += 1 
   end 
   new_array 
